b395775530044c73fcffaba6e75bec5daf9a05c1
ClaudioFritzen <sdfritzen96@gmail.com>
2022-08-23 00:32:19 +0100

    deixando dinamico o base.html

--- pessoa/models.py
+++ pessoa/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Pessoa(models.Model):
-    nome_completo = models.CharField(max_length=256)
+    nome_completo = models.CharField(max_length=256) 
     data_nascimento = models.DateField(null=True)
     ativa = models.BooleanField(default=True)
