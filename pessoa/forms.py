53bec57c1bdf66a72dacb9de1063546d983ce06c
ClaudioFritzen <sdfritzen96@gmail.com>
2022-08-24 00:41:25 +0100

    adcionando, removento, alterando e botao de buscar

--- pessoa/forms.py
+++ pessoa/forms.py
@@ -1,8 +1,14 @@
 from dataclasses import fields
-from django.forms import ModelForm
+from django.forms import ModelForm, fields, models
+from django import forms
 from .models import Pessoa
 
 class PessoaForm(ModelForm):
+    data_nascimento = forms.DateField(
+        widget=forms.TextInput(
+            attrs={"type": "date"}
+        )
+    )
     class Meta:
         model = Pessoa
         fields = ['nome_completo',  'data_nascimento', 'ativa']
