3ed81fd4d5f26f1a862edc9bd9169e9e4696481b
ClaudioFritzen <sdfritzen96@gmail.com>
2022-08-23 02:31:33 +0100

    criando formulario de pessoas

---
+++ pessoa/forms.py
@@ -0,0 +1,9 @@
+from dataclasses import fields
+from django.forms import ModelForm
+from .models import Pessoa
+
+class PessoaForm(ModelForm):
+    class Meta:
+        model = Pessoa
+        fields = ['nome_completo',  'data_nascimento', 'ativa']
+        # fields = ['__all__']
