26232fa2457fe8d86b65b186c09220a43c78c22e
ClaudioFritzen <sdfritzen96@gmail.com>
2022-08-26 11:15:55 +0100

    add funcionalidade (ativando e desativando usuario)

--- pessoa/admin.py
+++ pessoa/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 from .models import Pessoa, Contato
 # Register your models here.
 
+@admin.action(description='Ativar todas as pessoas')
+def ativar_todos(modeladmin, request, queryset):
+    queryset.update(ativa=True)
+
+
+@admin.action(description='Desativar todos')
+def desativar_todos(modeladmin, request, queryset):
+    queryset.update(ativa=False)
+
 class PessoaAdmin(admin.ModelAdmin):
     list_display = [
         'nome_completo',
@@ -15,6 +24,14 @@ class PessoaAdmin(admin.ModelAdmin):
     list_field = [
         'nome_completo'
     ]
+    actions = [
+        ativar_todos,
+        desativar_todos
+        
+    ]
+
+
+
 
 admin.site.register(Pessoa, PessoaAdmin)
 admin.site.register(Contato)
